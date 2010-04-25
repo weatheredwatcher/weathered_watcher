e6bb409db81bd8b50345eff24b9ec0c93ce0130d
David Duggins <weatheredwatcher@gmail.com>
2010-04-25 00:59:13 -0400

    more layout tweaks and wrote the initial code for the blog posts

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -13,13 +13,15 @@ class SiteController < ApplicationController
   end
 
   def blog
-    file = 'test.txt'
-    File.open(file, 'r') do |f1|  
-      while line = f1.gets  
-         puts line  
-      end  
-   end
-   
+   # This is where we parse and dump the output of the text files into the blog
+   #TODO Need to read a directory of files instead of a single file OR use XML for the entries
+   #TODO Need to create some structure for various blog elements
+    f = File.open("#{RAILS_ROOT}/public/test.txt", 'r')
+    @contentsArray=[]  # start with an empty array
+    f.each_line {|line|
+      @contentsArray.push line
+    }
+    
   end
 
 end
