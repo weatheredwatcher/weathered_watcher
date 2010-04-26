057274162b937942d08883d2c7bf4b469a9a4864
David Duggins <weatheredwatcher@gmail.com>
2010-04-26 10:49:01 -0400

    a few more style tweaks.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,18 +1,22 @@
 class SiteController < ApplicationController
   def index
-    @title="Main"
+    @title="Welcome to WeatheredWatcher"
   end
 
   def about
+    @title="Welcome to WeatheredWatcher::About"
   end
 
   def projects
+    @title="Welcome to WeatheredWatcher::Projects & Portfolio"
   end
 
   def contact
+    @title="Welcome to WeatheredWatcher::Contact"
   end
 
   def blog
+    @title="Welcome to WeatheredWatcher::Blog"
    # This is where we parse and dump the output of the text files into the blog
    #TODO Need to read a directory of files instead of a single file OR use XML for the entries
    #TODO Need to create some structure for various blog elements
