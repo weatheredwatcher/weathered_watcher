14073b46e06f8968301bd50b19e178094d9441ab
David Duggins <weatheredwatcher@gmail.com>
2010-05-07 12:48:34 -0400

    opps, made a tiny error, but this will fix it!

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -20,9 +20,9 @@ class SiteController < ApplicationController
    
     
     
-    This is where we parse and dump the output of the text files into the blog
-   TODO Need to read a directory of files instead of a single file OR use XML for the entries
-   TODO Need to create some structure for various blog elements
+   #This is where we parse and dump the output of the text files into the blog
+   #TODO Need to read a directory of files instead of a single file OR use XML for the entries
+   #TODO Need to create some structure for various blog elements
     f = File.open("#{RAILS_ROOT}/public/test.txt", 'r')
      @contentsArray=[]  # start with an empty array
     f.each_line {|line|
