bb181853f5455c4611fbb31ac2883c3ecfa9290b
David Duggins <weatheredwatcher@gmail.com>
2010-05-07 12:47:18 -0400

    going back to my placeholder on the live site while I play around with this some more...interesting stuff indeed!

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -17,24 +17,17 @@ class SiteController < ApplicationController
 
   def blog
     @title="Welcome to WeatheredWatcher::Blog"
-    httpauth = Twitter::HTTPAuth.new('davidduggins', 'm0rpheus')
-
-    client = Twitter::Base.new(httpauth)
-    #client.update('Heeeeyyyyooo from the Twitter Gem')
-    client.user_timeline.each { |tweet| puts tweet.text }
-    
-    #=> If you ever need speakers for a tech event, I suggest visiting here first: https://www.socialtext.net/speakers/index.cgi
-
+   
     
     
-   # This is where we parse and dump the output of the text files into the blog
-   #TODO Need to read a directory of files instead of a single file OR use XML for the entries
-   #TODO Need to create some structure for various blog elements
-   # f = File.open("#{RAILS_ROOT}/public/test.txt", 'r')
-   #  @contentsArray=[]  # start with an empty array
-   # f.each_line {|line|
-   #    @contentsArray.push line
-   # }
+    This is where we parse and dump the output of the text files into the blog
+   TODO Need to read a directory of files instead of a single file OR use XML for the entries
+   TODO Need to create some structure for various blog elements
+    f = File.open("#{RAILS_ROOT}/public/test.txt", 'r')
+     @contentsArray=[]  # start with an empty array
+    f.each_line {|line|
+       @contentsArray.push line
+    }
     
   end
 
