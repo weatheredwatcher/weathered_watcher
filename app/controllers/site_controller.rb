2772f0d3afecc1fd943099ddfd1190402d26fb6b
David Duggins <weatheredwatcher@gmail.com>
2010-05-07 12:27:55 -0400

    hm. Adding some code to pull twitter feeds

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -17,14 +17,29 @@ class SiteController < ApplicationController
 
   def blog
     @title="Welcome to WeatheredWatcher::Blog"
+    require 'rexml/document'
+    require 'open-uri'
+    include REXML
+
+    user = 'DavidDuggins' 
+    id = open("http://twitter.com/#{user}", "UserAgent" => "Ruby-Twitter Reader").read[/\d+(?=\.rss)/]
+
+    url = 'http://twitter.com/statuses/user_timeline/' + id + '.rss'
+    doc = Document.new(open(url, "UserAgent" => "Ruby-Twitter Reader").read)
+    puts doc.root.text('channel/item/title')[/^.[^:]+:\s+(.*)/,1]
+
+    #=> If you ever need speakers for a tech event, I suggest visiting here first: https://www.socialtext.net/speakers/index.cgi
+
+    
+    
    # This is where we parse and dump the output of the text files into the blog
    #TODO Need to read a directory of files instead of a single file OR use XML for the entries
    #TODO Need to create some structure for various blog elements
-    f = File.open("#{RAILS_ROOT}/public/test.txt", 'r')
-    @contentsArray=[]  # start with an empty array
-    f.each_line {|line|
-      @contentsArray.push line
-    }
+   # f = File.open("#{RAILS_ROOT}/public/test.txt", 'r')
+   #  @contentsArray=[]  # start with an empty array
+   # f.each_line {|line|
+   #    @contentsArray.push line
+   # }
     
   end
 
