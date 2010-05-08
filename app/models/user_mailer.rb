3534f8d1263909edb29f443dcc8c91719856b57e
David Duggins <weatheredwatcher@gmail.com>
2010-05-07 23:52:25 -0400

    removed some actionmailer code

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,13 +1,13 @@
 class UserMailer < ActionMailer::Base
 
-  def message_from_visitor(email = 'anon@example.net', message)
-    @subject = "Email sent via your website"
-    @body['message'] = message
-    @recipients = "youraddress@example.com"
-    @from = email
-    @sent_on = Time.now
-    @headers = {}
-  end
+#  def message_from_visitor(email = 'anon@example.net', message)
+#    @subject = "Email sent via your website"
+#    @body['message'] = message
+#    @recipients = "youraddress@example.com"
+#    @from = email
+#    @sent_on = Time.now
+#    @headers = {}
+#  end
     
 
 end
