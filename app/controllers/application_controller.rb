5cfb6001c0b62fd7df54035fbf2eff074a1dfb9a
David Duggins <weatheredwatcher@weatheredwatcher-laptop.(none)>
2010-10-13 13:18:09 -0400

    Added a last.fm feed reader for my website

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,3 +8,27 @@ class ApplicationController < ActionController::Base
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 end
+
+class GetLastFmFeed
+
+  def self.displayFeed
+
+     require 'rss'
+     require 'open-uri'
+     @lastfmArray = []
+     rss = RSS::Parser.parse open('http://ws.audioscrobbler.com/1.0/user/minasmir/recenttracks.rss').read, false
+
+     puts rss.channel.title
+     @lastfmArray.push "<p class='section'>Last.FM Feed</p>"
+     @lastfmArray.push "<ul style='font-size:11px;'>"
+     rss.items.each_with_index do |item,i|
+     puts "" if i.zero? or item.date.day != rss.items[i-1].date.day
+
+      line =  "<li><a href=\"#{item.link}\">#{item.title}</a></li> "
+     @lastfmArray.push line
+   end
+   @lastfmArray.push "</ul>"
+    return @lastfmArray
+  end
+
+end
--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -3,32 +3,36 @@ class SiteController < ApplicationController
     @title="Welcome to WeatheredWatcher"
     @current_page = "index"
     @jspromote = "<a href='https://developer.mozilla.org/en/JavaScript/Guide' title='Learning JavaScript'><img src='http://static.jsconf.us/promotejsh.gif' height='150' width='180' alt='Learning JavaScript'/></a>"
+    @lastfm_feed = GetLastFmFeed.displayFeed
   end
 
   def about
     @title="Welcome to WeatheredWatcher::About"
     @current_page = "about"
     @jspromote = "<a href='https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Number' title='JS Number .toString'><img src='http://static.jsconf.us/promotejsv.gif' height='280' width='160' alt='JS Number .toString'/></a>"
+    @lastfm_feed = GetLastFmFeed.displayFeed
   end
 
   def projects
     @title="Welcome to WeatheredWatcher::Projects & Portfolio"
     @current_page = "portfolio"
     @jspromote = "<a href='https://developer.mozilla.org/en/JavaScript' title='JavaScript API'><img src='http://static.jsconf.us/promotejsh.gif' height='150' width='180' alt='JavaScript API'/></a>"
+    @lastfm_feed = GetLastFmFeed.displayFeed
   end
 
   def contact
     @title="Welcome to WeatheredWatcher::Contact"
     @current_page = "contact"
     @jspromote = "<a href='https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array' title='JS Array length'><img src='http://static.jsconf.us/promotejsh.gif' height='150' width='180' alt='JS Array length'/></a>"
-
+    @lastfm_feed = GetLastFmFeed.displayFeed
   end
 
   def blog
   @title="Welcome to WeatheredWatcher::Blog"
   @current_page = "blog"
   @jspromote = "<a href='https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/RegExp' title='JavaScript RegExp Example: Using a regular expression with the sticky flag'><img src='http://static.jsconf.us/promotejsh.gif' height='150' width='180' alt='JavaScript RegExp Example: Using a regular expression with the sticky flag'/></a>"
-  require 'rss'
+  @lastfm_feed = GetLastFmFeed.displayFeed
+    require 'rss'
   require 'open-uri'
 @contentArray = []
   rss = RSS::Parser.parse open('http://weatheredwatcher.posterous.com/rss.xml').read, false
